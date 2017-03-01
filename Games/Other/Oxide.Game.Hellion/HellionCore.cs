2cff5077df16bcf827263593d1f7042648bc6e8c
Jonathan Lindahl <jonathanjl99@gmail.com>
2017-03-01 23:29:31 +0100

    HellionCore initial testing

--- Games/Other/Oxide.Game.Hellion/HellionCore.cs
+++ Games/Other/Oxide.Game.Hellion/HellionCore.cs
@@ -1,4 +1,11 @@
-﻿using Oxide.Core.Plugins;
+﻿using Oxide.Core;
+using Oxide.Core.Logging;
+using Oxide.Core.Plugins;
+using Oxide.Game.Hellion.Extensions;
+using Oxide.Plugins;
+using ZeroGravity;
+using ZeroGravity.Network;
+using ZeroGravity.Objects;
 
 namespace Oxide.Game.Hellion
 {
@@ -7,6 +14,51 @@ namespace Oxide.Game.Hellion
     /// </summary>
     public class HellionCore : CSPlugin
     {
+        protected Server Server { get; private set; }
 
+        public HellionCore()
+        {
+            Author = "Skipcast";
+
+            Interface.Oxide.RootLogger.AddLogger(new CallbackLogger(message =>
+            {
+                Dbg.Info(message);
+            }));
+        }
+
+        [HookMethod("IOnStart")]
+        private void base_OnStart(Server server)
+        {
+            Server = server;
+            Dbg.Info("OnStart");
+            CallHook("OnStart", server);
+        }
+
+        [HookMethod("IOnSaved")]
+        private void base_OnSaved()
+        {
+            Dbg.Info("OnSaved");
+            CallHook("OnSaved");
+        }
+
+        [HookMethod("IOnTextChat")]
+        private void base_OnTextChat(TextChatMessage chatMessage, bool local)
+        {
+            Dbg.Info($"OnTextChat: {chatMessage.MessageText} ({chatMessage.GetPlayer().Name}) local: {local}");
+            CallHook("OnTextChat", chatMessage, local);
+        }
+
+        [HookMethod("IOnSpawnStartingModule")]
+        private void base_OnSpawnStartingModule(Player player, Ship ship)
+        {
+            Dbg.Info($"OnSpawnStartingModule for player ${player.Name}");
+            CallHook("OnSpawnStartingModule", player, ship);
+        }
+
+        [HookMethod("IOnLoginPlayer")]
+        private void base_OnLoginPlayer(EventSystem.InternalEventData data)
+        {
+            
+        }
     }
 }
