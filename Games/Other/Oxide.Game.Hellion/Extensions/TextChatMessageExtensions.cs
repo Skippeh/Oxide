22e911cb63fe8190ee8ee36829e6c8873f6cdb74
Jonathan Lindahl <jonathanjl99@gmail.com>
2017-03-01 23:32:12 +0100

    TextChatMessage extension class, GetPlayer extension

---
+++ Games/Other/Oxide.Game.Hellion/Extensions/TextChatMessageExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using ZeroGravity;
+using ZeroGravity.Network;
+using ZeroGravity.Objects;
+
+namespace Oxide.Game.Hellion.Extensions
+{
+    public static class TextChatMessageExtensions
+    {
+        public static Player GetPlayer(this TextChatMessage chatMessage)
+        {
+            if (chatMessage == null) throw new ArgumentNullException(nameof(chatMessage));
+            return Server.Instance.GetPlayer(chatMessage.GUID);
+        }
+    }
+}
